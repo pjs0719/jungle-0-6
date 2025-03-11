761108d97f7087ea98c454954f865fbf08576c60
박지성 <pjssoj07193@gmail.com>
2025-03-11 23:59:13 +0900

    파일 정리

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import calendar
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://jisung719.synology.me:27017')
 db = client.dbjungle
 diary_collection = db.diary
 
